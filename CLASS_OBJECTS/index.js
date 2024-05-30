525f063efbf846b858d15e97235288970f52deea
Fantine <fantine.rudent@hotmail.fr>
2024-05-30 09:57:21 +0200

    exo class ok

---
+++ CLASS_OBJECTS/index.js
@@ -0,0 +1,11 @@
+// Création de Classes et Objets
+// Exercice : Gestion d'une bibliothèque
+
+// Objectif : Créer une classe Book pour représenter un livre avec des propriétés comme title, author, year et des méthodes pour obtenir des informations sur le livre.
+
+// Instructions :
+
+// Créez une classe Book.
+// Ajoutez un constructeur qui initialise les propriétés title, author, et year.
+// Ajoutez une méthode getSummary qui retourne un résumé du livre.
+// Créez plusieurs instances de la classe Book et affichez leur résumé.
